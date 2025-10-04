ade799970659af65cc57529ac89e109afb279725
Omer Prizner <omer.prizner@gmail.com>
2025-10-04 15:27:29 +0300

    simplify -3- + some instructions and notes

--- src/routes/fishy/[...paths]/List.tsx
+++ src/routes/fishy/[...paths]/List.tsx
@@ -1,6 +1,6 @@
-import { component$, useContext, useStyles$ } from '@builder.io/qwik';
-import { Context } from '../context';
-import styles from './List.css?inline';
+import { component$, useContext, useStyles$ } from "@builder.io/qwik";
+import { Context } from "../context";
+import styles from "./List.css?inline";
 
 export const List = component$(() => {
   useStyles$(styles);
@@ -11,20 +11,28 @@ export const List = component$(() => {
       <div class="flex justify-between gap-3">
         <div class="mt-[20px]">
           <h2 class="text-[2rem]">
-            {/* Qwik Buggy so we need to use span to get reactivity... */}
+            {weirdPart(context.routeParams)}
+            {/* Also - Wrapping this text node with a span, would make the bug go away. */}
+            {/* <span>{context.data.teachers.length}</span> */}
             {context.data.teachers.length}
-            {test(context.routeParams)}
           </h2>
         </div>
       </div>
       <div class="mt-12 flex flex-1 flex-wrap items-start justify-between gap-y-5 max-md:mt-8">
-        <div class="teacher-list-container"></div>
+        <div class="teacher-list-container">
+          {JSON.stringify(context.data.teachers)}
+        </div>
       </div>
     </>
   );
 });
 
-function test(x) {
-  const { instrumentId, cityName } = x;
-  return 'מורים';
+// Only when I destructure the object, extracting the param I modify before the navigation, the bug is reproduced.
+function weirdPart(x: any) {
+  const { cityName } = x;
+
+  // Just to clarify even if we return a string that doesnt include the cityName, the bug is reproduced.
+  // e.g >> return "מורים";
+
+  return `Teachers in ${cityName}: `;
 }
